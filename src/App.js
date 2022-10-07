a9973e0ec85d71b80515bab3888acb71bd3d4a05
ogola5 <ogolaevance5@gmail.com>
2022-10-07 11:26:02 +0300

    completed assignment

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import HomePage from './components/Home';
+
+function App() {
+  return (
+    <div className="container">
+      
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Home.js
@@ -0,0 +1,12 @@
+import React from "react";
+import {Link} from 'react-router-dom'
+
+function HomePage(){
+    return(
+        <div>
+            <h1>welcome</h1>
+            <link to="/items">Join us </link>
+        </div>
+    )
+}
+export default HomePage
