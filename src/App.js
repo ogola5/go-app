2bfe03e28e501badea4ea639d1d150c217f6c317
ogola5 <ogolaevance5@gmail.com>
2022-10-07 11:34:24 +0300

    completed assignment

--- src/App.js
+++ src/App.js
@@ -1,13 +1,30 @@
 import React from 'react';
 import './App.css';
-import HomePage from './components/Home';
 
-function App() {
-  return (
-    <div className="container">
-      
-    </div>
-  );
+import HomePage from './components/HomePage'
+import ProductsContainer from './ProductsContainer'
+import ProductPage from './components/ProductPage'
+
+import { BrowserRouter, Route } from 'react-router-dom';
+
+class App extends React.Component {
+
+
+  render() {
+    return (
+      <div className="container">
+        <h1>Ecommerce Shopping Cart Application</h1>
+        <br />
+        <BrowserRouter>
+        
+        <Route exact path="/" component={HomePage} />
+        <Route exact path="/products" component={ProductsContainer} />
+        <Route exact path="/product/:id" render={props => ( <ProductPage { ...props } id={this.state} />  )} />
+        
+        </BrowserRouter>
+      </div>
+    );
+  }
 }
 
 export default App;
